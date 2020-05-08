f03a1c9a30e9d73dff03391b8c9733ed8dc1c3a5
Caleb Chang <changc42@gmail.com>
2020-05-08 10:15:34 -0400

    completed database design. now starting queries

--- project3/sql_commands.sql
+++ project3/sql_commands.sql
@@ -94,22 +94,123 @@ alter table specific_product
 add product_rating_date date
 
 alter table specific_product
-add constraint rate_only_if_purchase
-    check (NOT(
-				(purchase_id is null and product_rating is not null) or 
-				(product_rating is null and (product_rating_comment is not null or product_rating_date is not null))
-		))
+add product_rating_method varchar(20) default 'number system'
+
+-- alter table specific_product
+-- add constraint rate_only_if_purchase
+--     check (NOT(
+-- 				(purchase_id is null and product_rating is not null) or 
+-- 				(product_rating is null and (product_rating_comment is not null or product_rating_date is not null))
+-- 		))
 
 alter table purchase
-add (delivery_rating varchar(30), delivery_rating_comment varchar(30), delivery_rating_date date)
+add (
+	delivery_rating varchar(30), 
+	delivery_rating_comment varchar(30), 
+	delivery_rating_date date,
+	method_of_rating varchar(30) default 'number system',
+	tip number(9,2)
+)
+
+-- alter table purchase
+-- add constraint rate_only_if_purchase
+--     check (NOT(
+-- 				(purchase_id is null and product_rating is not null) or 
+-- 				(product_rating is null and (product_rating_comment is not null or product_rating_date is not null))
+-- 		))
 
 alter table purchase
-add constraint rate_only_if_purchase
-    check (NOT(
-				(purchase_id is null and product_rating is not null) or 
-				(product_rating is null and (product_rating_comment is not null or product_rating_date is not null))
-		))
+rename column method to  method_of_payment
 
-member = product
-employer = purchase
-location rating
+create table method_of_payment
+(
+    method_of_payment varchar(30) primary key
+)
+
+
+create table customer_method_of_payment
+(
+    cust_email varchar(20),
+    method_of_payment varchar(30),
+    card_number number(16),
+    exp_date date,
+    constraint pk_cust_method_of_payment
+        primary key(cust_email, method_of_payment, card_number),
+    constraint fk_cust_email
+        foreign key(cust_email)
+        references customer(cust_email),
+		constraint fk_method_of_payment
+        foreign key(method_of_payment)
+        references method_of_payment(method_of_payment)
+)
+
+insert into method_of_payment
+values('cash')
+
+insert into method_of_payment
+values('credit')
+
+insert into method_of_payment
+values('debit')
+
+alter table purchase
+add constraint fk_purchase__method_of_payment
+    foreign key(method_of_payment)
+    references method_of_payment(method_of_payment)
+
+create sequence first_seq
+    start with 1000
+    increment by 1
+
+grant select on purchase to customer
+
+-- select * from specific_product
+-- where product_rating_comment like '%[1:9][1:9][1:9]-[1:9][1:9][1:9]-[1:9][1:9][1:9][1:9]%' or
+-- 	product_rating_comment like '%@%.com' or
+-- 	product_rating_comment like '%street%' or
+-- 	product_rating_comment like '%[1:9][1:9]/[1:9][1:9]/[1:9][1:9]%'
+
+alter table purchase
+modify (delivery_rating_comment varchar(150))
+
+update purchase
+set delivery_rating = '5', delivery_rating_comment = '123-576-4334 is my number. the delivery was good', delivery_rating_date='08-MAY-20', tip=2.00
+where purchase_id = 1
+
+
+update purchase
+set delivery_rating = '4', delivery_rating_comment = 'apooli@gmail.com. the delivery was ok', delivery_rating_date='07-MAY-20', tip = 3.50
+where purchase_id = 2
+
+
+update purchase
+set delivery_rating = '3', delivery_rating_comment = '352-335 stilop street. the delivery was ok', delivery_rating_date='07-MAY-20', tip=1.00
+where purchase_id = 3
+
+update purchase
+set delivery_rating = '2', delivery_rating_comment = '324-363-7894. the delivery was bad', delivery_rating_date='06-MAY-20', tip=0.10
+where purchase_id = 4
+
+update purchase
+set delivery_rating = '1', delivery_rating_comment = 'completely awful. guilo@pillo.com', delivery_rating_date='05-MAY-20', tip=0.01
+where purchase_id = 5
+
+update specific_product
+set product_rating='5', product_rating_comment='this orange was delicious. my birthday is 01/21/95', product_rating_date='01-APR-20'
+where product_id='1'
+
+update specific_product
+set product_rating='1', product_rating_comment='this banana was awful.', product_rating_date='02-APR-20'
+where product_id='10'
+
+update specific_product
+set product_rating='3', product_rating_comment='this starfruit was okay.', product_rating_date='03-APR-20'
+where product_id='22'
+
+update specific_product
+set product_rating='5', product_rating_comment='I had a great day today', product_rating_date='04-APR-20'
+where product_id='23'
+
+update specific_product
+set product_rating='4', product_rating_comment='My neighbor is making a lot of noise', product_rating_date='05-APR-20'
+where product_id='30'
